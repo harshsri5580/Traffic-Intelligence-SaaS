deeb77234d9c6dd253e8314dc043d9f12052e1e0
harsh6390 <hs07052000@gmail.com>
2026-03-26 12:26:34 +0530

    final build fix

--- frontend/src/app/dashboard/campaigns/edit/page.js
+++ frontend/src/app/dashboard/campaigns/edit/page.js
@@ -1,9 +1,6 @@
-import dynamic from "next/dynamic";
+import dynamicImport from "next/dynamic";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-const EditClient = dynamic(() => import("./EditClient"), {
+const EditClient = dynamicImport(() => import("./EditClient"), {
   ssr: false,
 });
 
