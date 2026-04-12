33d0cc05f0325bcef487911d5d129dbf25b2e0a2
harsh6390 <hs07052000@gmail.com>
2026-04-12 16:22:18 +0530

    paddle production dashbord firp

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL
-  ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+const cleanURL = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "");
+
+const baseURL = cleanURL
+  ? `${cleanURL}/api`
   : "http://127.0.0.1:8000/api";
 
 const api = axios.create({
